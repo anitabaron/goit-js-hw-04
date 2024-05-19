7a5a2309bfe3f4191dbbb41094419a6994d134af
anitabaron <111062951+anitabaron@users.noreply.github.com>
2024-05-19 19:17:44 +0200

    update tasks

---
+++ js/task-2.js
@@ -0,0 +1,38 @@
+function calcAverageCalories(days) {
+  let allValues = [];
+  let sumValues = 0;
+  for (const day of days) {
+    allValues.push(day.calories);
+  }
+  for (const i of allValues) {
+    sumValues += i;
+  }
+  return sumValues / 7;
+  // return sumValues / days.length;   //zwraca Nan dla []
+}
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 3010 },
+    { day: "tuesday", calories: 3200 },
+    { day: "wednesday", calories: 3120 },
+    { day: "thursday", calories: 2900 },
+    { day: "friday", calories: 3450 },
+    { day: "saturday", calories: 3280 },
+    { day: "sunday", calories: 3300 },
+  ])
+); // 3180
+
+console.log(
+  calcAverageCalories([
+    { day: "monday", calories: 2040 },
+    { day: "tuesday", calories: 2270 },
+    { day: "wednesday", calories: 2420 },
+    { day: "thursday", calories: 1900 },
+    { day: "friday", calories: 2370 },
+    { day: "saturday", calories: 2280 },
+    { day: "sunday", calories: 2610 },
+  ])
+); // 2270
+
+console.log(calcAverageCalories([])); // 0
